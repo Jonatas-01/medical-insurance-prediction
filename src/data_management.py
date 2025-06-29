e5770fdbe06077887d169167961c5b77221f2471
JonatasM <jonatasmc02@gmail.com>
2025-06-29 19:42:14 +0000

    format src python code based on pep8

--- src/data_management.py
+++ src/data_management.py
@@ -2,14 +2,17 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+
 # load data
 @st.cache_data
 def load_data():
     df = pd.read_csv('outputs/datasets/collection/insurance.csv')
     return df
 
+
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
+
 
 def load_feature_engineering_data():
     df = pd.read_csv('outputs/datasets/feature_engineered/insurance_fe.csv')
--- src/machine_learning/predict_insurance.py
+++ src/machine_learning/predict_insurance.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
+
 def predict_insurance(X_live, insurance_features, insurance_pipe_dc_fe, insurance_pipe_model):
     """
     Predicts insurance charges based on the input features.
-    
+
     Parameters:
     - X_live: DataFrame containing the input features for prediction.
     - insurance_features: List of feature names used in the model.
     - insurance_pipe_dc_fe: Data cleaning and feature engineering pipeline.
     - insurance_pipe_model: Trained machine learning model pipeline.
-    
+
     Returns:
     - insurance_prediction: Predicted insurance charges.
     """
@@ -19,11 +20,11 @@ def predict_insurance(X_live, insurance_features, insurance_pipe_dc_fe, insuranc
 
     # Apply data cleaning and feature engineering
     X_live_insurance_dc_fe = insurance_pipe_dc_fe.transform(X_live_insurance)
-    
+
     # Make predictions using the trained model
     insurance_prediction = insurance_pipe_model.predict(X_live_insurance_dc_fe)
-    
+
     # Display the prediction result
     st.success(f"Predicted Insurance Charges: ${insurance_prediction[0]:,.2f}")
-    
+
     return insurance_prediction
